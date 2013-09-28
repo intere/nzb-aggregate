676c15b678312b1bdc4aea486efe8480194e767a
Eric Internicola <einternicola@silverchalice.com>
2013-09-28 11:23:46 -0600

    More work on the NZB Search.

--- angular-ui/app/scripts/controllers/NzbSearch.js
+++ angular-ui/app/scripts/controllers/NzbSearch.js
@@ -3,30 +3,31 @@
 angular.module('nzbUiApp')
   .controller('NzbSearchCtrl', function ($scope, $log, NzbService) {
 
-  	$scope.maxResults = 500;
+  	$scope.maxResults = 100;
+  	$scope.selectedItems = [];
   	$scope.results = [];
   	$scope.decoratedResults = undefined;
 
- //  	// Grid Options:
-	// $scope.gridOptions = {
-	//     data: 'results',
-	//     columnDefs: [
-	//     	{field:'artworkUrl60',displayName:'Cover',cellClass:'album-cover',cellTemplate:'<img ng-src="{{COL_FIELD}}">'},
-	// 	    {field:'artistName', displayName:'Artist'},
-	// 	    {field:'collectionName', displayName:'Album'},
-	// 	    {field:'trackName', displayName: 'Track Name'},
-	// 	    {field:'releaseDate', displayName:'Released', cellTemplate:'<span ng-cell-text>{{convertDate(COL_FIELD)}}</span>',cellClass:'date'},
-	// 	    {field:'trackCount', displayName:'Tracks'},
-	// 	    {field:'primaryGenreName', displayName:'Genre'},
-	// 	    {field:'previewUrl', displayName:'Preview', cellTemplate:'<a ng-href="{{COL_FIELD}}" target="_new" ng-hide="!COL_FIELD">Listen</a>',cellClass:'link'}
-	// 	],
-	// 	showFooter: true,
-	// 	totalServerItems: 0,
-	// 	selectedItems: $scope.selectedItems,
-	// 	multiSelect: false,
-	// 	enableColumnResize: true,
-	// 	rowHeight: 60
-	// };
+  	// Grid Options:
+	$scope.gridOptions = {
+	    data: 'results',
+	    columnDefs: [
+	    	{field:'collection',displayName:'',cellClass:'collection',cellTemplate:'<i class="icon-th" ng-show="COL_FIELD"></i>', width: '20'},
+		    {field:'subject', displayName:'Subject', width: "800"},
+		    {field:'group', displayName:'Group'},
+		    {field:'poster', displayName: 'Poster'},
+		    {field:'size', displayName: 'Size', cellTemplate: '<div ng-cell-text>{{COL_FIELD.size}} {{COL_FIELD.unit}}</div>', width: '100'}
+		    // {field:'', displayName: ''},
+
+		],
+		showFooter: true,
+		totalServerItems: 0,
+		selectedItems: $scope.selectedItems,
+		multiSelect: true,
+		enableColumnResize: true,
+		showFilter: true,
+		rowHeight: 60
+	};
 
   	$scope.getMaxResults = function() {
   		return [50, 100, 250, 500, 1000, 2500, 5000];
@@ -35,6 +36,7 @@ angular.module('nzbUiApp')
   	$scope.nzbSearch = function() {
   		NzbService.doSearch($scope.searchText, $scope.maxResults, $scope.startIndex).then(
   			function(results){
+  				$scope.selectedItems.length = 0;
   				$scope.decoratedResults = results;
   				$scope.results = results.flattenResults();
   				console.dir(results);
